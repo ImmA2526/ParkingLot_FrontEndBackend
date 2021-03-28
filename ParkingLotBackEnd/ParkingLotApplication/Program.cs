ae31bd3ad52550e999256fd4a21cf8d4f65a815e
Imran2526 <imraninfo97@gmail.com>
2021-03-28 21:14:05 +0530

    [Imran] Refactored : PAckage json Refactored

--- ParkingLotBackEnd/ParkingLotApplication/Program.cs
+++ ParkingLotBackEnd/ParkingLotApplication/Program.cs
@@ -1,12 +1,5 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ParkingLotApplication
 {
@@ -22,3 +15,6 @@ namespace ParkingLotApplication
                 .UseStartup<Startup>();
     }
 }
+
+
+
